62b43bc7cea49dbca70c53dc73c95caa5bfe6e10
wanglintao <wanglintao@1fangxin.cn>
2018-12-14 13:40:30 +0800

    hello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    hello
                 </div>
 
                 <div class="links">
