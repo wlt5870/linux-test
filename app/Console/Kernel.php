f3514560387b302c031c93cbcccf9c0cde76df1f
wlt5870 <wlt5870@163.com>
2019-04-14 01:49:19 +0800

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->call(function(){
-             file_put_contents(app_path(). 'command.txt', '23', FILE_APPEND);
+             file_put_contents(app_path(). DIRECTORY_SEPARATOR .'command.txt', '23', FILE_APPEND);
          })
                   ->everyMinute();
     }
